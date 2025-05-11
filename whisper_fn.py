860b44a2c69d95f3f97ebdddfdcfebfc4fb4ff21
Pikacnu <a13456123456@gmail.com>
2025-05-11 21:16:36 +0800

    feat: update transcribe_audio to include regroup option in transcription

--- whisper_fn.py
+++ whisper_fn.py
@@ -6,6 +6,7 @@ async def transcribe_audio(video_id: str, model_name: str = 'medium'):
 
     aduio_file_path = f'./downloads/audio/{video_id}.mp3'
 
-    result = model.transcribe(aduio_file_path, vad=True, denoiser="demucs")
-    result = model.align(aduio_file_path, result)
+    result = model.transcribe(aduio_file_path, vad=True, denoiser="demucs", regroup=True)
+    
+    result=model.align(aduio_file_path, result)
     result.save_as_json(f'./downloads/lyrics/{video_id}.json')
